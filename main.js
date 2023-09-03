bec9ff7c760f375b03b8b78917a6a5eb386f6215
moudapro <97889018+moudapro@users.noreply.github.com>
2023-09-03 08:05:28 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ let cel = document.getElementById('cel');
 let nums = document.querySelectorAll('.num');
 nums.forEach((num)=>{
     num.addEventListener('input',()=>{
-        if(num){
-            if(num.id==='cel'){
+        if(num){/*if num is true*/
+            if(num.id==='cel'){/*if you enter the value in celius input*/
                 fah.value=(Number(num.value)*9/5)+32
                 kel.value=Number(num.value)+273.15
-            }else if(num.id==='kel'){
+            }else if(num.id==='kel'){/*if you enter the value in kelvin input*/
                 cel.value=Number(num.value)-273.15
                 fah.value=(Number(num.value)-273.15)*9/5+32
-            }else if(num.id==='fah'){
+            }else if(num.id==='fah'){/*/*if you enter the value in fahrenheit input*/
                cel.value=(Number(num.value)-32)*5/9
                kel.value=(Number(num.value)-32)*5/9+273.15
             }
